e2b94f90ca553c797dfc7a445842bcad2b322187
KiSeungMin <ksm011212@gmail.com>
2023-11-13 11:24:36 +0900

    chapter5

--- src/main/java/springBook/demo/Member.java
+++ src/main/java/springBook/demo/Member.java
@@ -1,11 +1,12 @@
 package springBook.demo;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Getter
 @Entity
